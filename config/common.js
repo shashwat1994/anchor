dc383b2322a4a3e3962311235332e18a586d08bf
shashwat1994 <shashwat.aapinventiv@gmail.com>
2018-01-08 18:24:56 +0530

    initial commit

---
+++ config/common.js
@@ -0,0 +1,31 @@
+module.exports.common = {
+
+
+  imgegeContainer: '/home/admin1/filesholder/',
+
+  supportEmailId: 'bitwire.io@gmail.com', //Support Email Id to send mail signup and login and forgot password
+  supportEmailIdpass: 'bitwire@io', //Support Email Id passoword
+
+  supportEmailIdService: 'gmail',
+  projectURL: 'http://192.168.0.100:1338',
+  txFeeBCHWithdrawSuccess: 0.0001,
+  txFeeBTCWithdrawSuccess: 0.0001,
+  txFeePYYWithdrawSuccess: 0.0001,
+  txFeeGDSWithdrawSuccess: 0.0001,
+  companyINRWAccount: "admin@gmail.com",
+  companyUSDWAccount: "admin@gmail.com",
+  companyEURWAccount: "admin@gmail.com",
+  companyGBPWAccount: "admin@gmail.com",
+  companyBRLWAccount: "admin@gmail.com",
+  companyPLNWAccount: "admin@gmail.com",
+  companyCADWAccount: "admin@gmail.com",
+  companyTRYWAccount: "admin@gmail.com",
+  companyRUBWAccount: "admin@gmail.com",
+  companyMXNWAccount: "admin@gmail.com",
+  companyCZKWAccount: "admin@gmail.com",
+  companyILSWAccount: "admin@gmail.com",
+  companyNZDWAccount: "admin@gmail.com",
+  companyJPYWAccount: "admin@gmail.com",
+  companySEKWAccount: "admin@gmail.com",
+  companyAUDWAccount: "admin@gmail.com",
+}
